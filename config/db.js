e829063ca566a5f5c64652d59ee99ded7bc836e0
Ansal Jain <jainansal@gmail.com>
2023-08-26 19:10:41 +0530

    fix: minor fixes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () => {
+  console.log('...Connecting Database...')
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL)
     console.log(`Mongo DB connected: ${connection.connection.host}`)
--- router/authRoutes.js
+++ router/authRoutes.js
@@ -4,14 +4,10 @@ import {
   authLogin,
   authRegister,
   authLogout,
-  authGetId
 } from "../controllers/authControllers.js"
 import { verifyToken } from "../middleware/verifyToken.js"
 
 export const authRoutes = express.Router()
-
-// Authorize user from JWT
-authRoutes.get('/authorize', verifyToken, authGetId)
 
 // Login
 authRoutes.post('/login', authLogin)
