72facc8e907ba041ccca602a0921bc5cc9d1ef40
Ansal Jain <jainansal@gmail.com>
2023-08-27 19:20:53 +0530

    update: fix routing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import history from "connect-history-api-fallback"
 
 // Local imports
 
@@ -22,6 +23,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+app.use(history())
 
 // Routes
 
