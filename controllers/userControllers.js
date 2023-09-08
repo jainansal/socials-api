1e163ac4f9433b44add6643edc6fd93fc9401f48
Ansal Jain <jainansal@gmail.com>
2023-09-08 23:57:26 +0530

    upd: populate friends with name and pfp

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -80,14 +80,17 @@ export const getUserFriends = async (req, res) => {
 
     const givenId = req.params.id;
     const givenUser = await User.findById(givenId)
-      .select("friends");
+      .populate({
+        path: 'friends',
+        select: 'name pfp'
+      })
 
     if (!givenUser) {
       res.status(404)
       throw new Error("User doesn't exist.");
     }
 
-    res.status(200).json(givenUser);
+    res.status(200).json(givenUser.friends);
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
